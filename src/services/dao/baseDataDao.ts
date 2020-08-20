764e65b17feba91519a79f40f2ced03864eeffc1
chenheng <chenheng@chenhengdeMacBook-Pro.local>
2020-08-20 15:35:29 +0800

    aaa

--- src/services/dao/baseDataDao.ts
+++ src/services/dao/baseDataDao.ts
@@ -1,5 +1,6 @@
 import BaseUser from "@/models/entity/BaseUser";
 import {CommonExcludeAttributes} from "@/constans/global";
+import {dbCtx} from "@/server/db/db_context";
 
 export const bulkCreateBaseUsers = async (records: any[]) => {
     const res = await BaseUser.bulkCreate(records);
@@ -15,9 +16,35 @@ export const findOneInCondition = async (params) => {
 }
 
 export const findAllInCondition = async (params) => {
-    return await BaseUser.findAll({
-        attributes: {exclude: [...CommonExcludeAttributes]},
-        raw: true,
-        where: params
-    })
+    if (params.role === "3"){
+        const db = dbCtx();
+        return await db.query(
+            `
+            select
+            CONCAT(b.id,s.id) as id,
+            b.name,b.phone,
+            ps.student_phone AS studentPhone,
+            s.name AS studentName
+            from
+            base_user b
+            INNER JOIN
+            parent_student ps
+            on b.role='3' and b.phone=ps.phone
+            LEFT JOIN
+            base_user s
+            on ps.student_phone=s.phone
+            `,
+            {
+                type: db.QueryTypes.SELECT,
+                plain: false,
+                raw: true,
+            }
+        )
+    }else{
+        return await BaseUser.findAll({
+            attributes: {exclude: [...CommonExcludeAttributes]},
+            raw: true,
+            where: params
+        })
+    }
 }
