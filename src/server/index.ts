776bdc45601fc02d38b5efd7cb7f0c58bed7bf3d
chenheng <chenheng@chenhengdeMacBook-Pro.local>
2020-11-04 15:00:35 +0800

    1

--- src/server/index.ts
+++ src/server/index.ts
@@ -32,7 +32,17 @@ if (process.env.NODE_ENV === ENV_PROD){
         app.use(enforceHttps.default());
     })
 }
-app.use(serve(path.join(process.cwd(), "/src/public/")));
+/**
+ * maxage浏览器缓存的最大寿命（以毫秒为单位）。默认为0
+ * hidden允许传输隐藏文件。默认为false
+ * index 默认文件名，默认为“ index.html”
+ * defer如果为true，则在服务之后return next()，允许任何下游中间件首先响应。
+ * gzip 当客户端支持gzip且所请求的扩展名为.gz的文件存在时，请尝试自动提供文件的gzip压缩版本。默认为true。
+ * br 当客户端支持brotli并且存在所请求的扩展名为.br的文件时，请尝试自动提供文件的brotli版本（请注意，仅通过https接受brotli）。默认为true。
+ * setHeaders函数，用于在响应时设置自定义标头。
+ * extensionsURL中没有扩展名时，尝试匹配传递的数组中的扩展名以搜索文件。首次发现是送达的。（默认为false）
+ */
+app.use(serve(path.join(process.cwd(), "/src/public/"), {maxage: 30 * 24 * 3600 * 1000}));
 app.use(cors());
 app.use(httpLogger());
 app.use(verify());
